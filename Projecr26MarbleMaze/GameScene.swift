92bd05c2f95af7afd7f3ddd4b1740a9b830c34e2
Igor Polousov <igorpolousov@gmail.com>
2022-03-18 14:53:30 +0300

    + scoreLabel

--- Projecr26MarbleMaze/GameScene.swift
+++ Projecr26MarbleMaze/GameScene.swift
@@ -23,12 +23,26 @@ class GameScene: SKScene {
     
     var motionManager: CMMotionManager?
     
+    var scoreLabel: SKLabelNode!
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.zPosition = 2
+        addChild(scoreLabel)
         
         loadLevel()
         createPlayer()
